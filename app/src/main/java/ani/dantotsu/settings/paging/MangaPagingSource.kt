6b8220131b935aff1b6bb0adc1c6c962d00a469f
rebelonion <87634197+rebelonion@users.noreply.github.com>
2023-11-17 15:10:10 -0600

    get rid of companion object (todo)

--- app/src/main/java/ani/dantotsu/settings/paging/MangaPagingSource.kt
+++ app/src/main/java/ani/dantotsu/settings/paging/MangaPagingSource.kt
@@ -87,7 +87,7 @@ class MangaExtensionPagingSource(
         } else {
             availableExtensions.filter { it.name.contains(query, ignoreCase = true) }
         }
-        val filternfsw = if(SettingsActivity.isNsfwEnabled) {
+        val filternfsw = if(SettingsActivity.isNsfwEnabled) {//TODO
             filteredExtensions
         } else {
             filteredExtensions.filterNot { it.isNsfw }
