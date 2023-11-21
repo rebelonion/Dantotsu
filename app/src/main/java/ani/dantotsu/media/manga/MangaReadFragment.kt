8d9254140deb91b1e8f76ead7d873f4a362ae6ec
Finnley Somdahl <87634197+rebelonion@users.noreply.github.com>
2023-11-21 03:01:11 -0600

    quickfix

--- app/src/main/java/ani/dantotsu/media/manga/MangaReadFragment.kt
+++ app/src/main/java/ani/dantotsu/media/manga/MangaReadFragment.kt
@@ -466,20 +466,25 @@ open class MangaReadFragment : Fragment(), ScanlatorSelectionListener  {
                     val chapterNumber = intent.getStringExtra(EXTRA_CHAPTER_NUMBER)
                     chapterNumber?.let { chapterAdapter.startDownload(it) }
                 }
+
                 ACTION_DOWNLOAD_FINISHED -> {
                     val chapterNumber = intent.getStringExtra(EXTRA_CHAPTER_NUMBER)
                     chapterNumber?.let { chapterAdapter.stopDownload(it) }
                 }
+
                 ACTION_DOWNLOAD_FAILED -> {
                     val chapterNumber = intent.getStringExtra(EXTRA_CHAPTER_NUMBER)
                     chapterNumber?.let {
                         chapterAdapter.removeDownload(it)
                     }
                 }
+
                 ACTION_DOWNLOAD_PROGRESS -> {
                     val chapterNumber = intent.getStringExtra(EXTRA_CHAPTER_NUMBER)
                     val progress = intent.getIntExtra("progress", 0)
-                    chapterNumber?.let { chapterAdapter.updateDownloadProgress(it, progress) 
+                    chapterNumber?.let {
+                        chapterAdapter.updateDownloadProgress(it, progress)
+                    }
                 }
             }
         }
