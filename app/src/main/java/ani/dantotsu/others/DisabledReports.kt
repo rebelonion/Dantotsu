2c0d698ac9d5937164709bce0459f7b0a969fa72
Sadwhy <99601717+Sadwhy@users.noreply.github.com>
2023-12-10 11:29:58 +0600

    Tracking denied

--- app/src/main/java/ani/dantotsu/others/DisabledReports.kt
+++ app/src/main/java/ani/dantotsu/others/DisabledReports.kt
@@ -1,4 +1,4 @@
 package ani.dantotsu.others
 
-const val DisabledReports = false
+const val DisabledReports = true
 //Setting this to false, will allow sending crash reports to Dantotsu's Firebase Crashlytics
