b14509c541ac238a984f54b1f8f6f7d976ebb7bd
Sadwhy <99601717+Sadwhy@users.noreply.github.com>
2023-12-11 10:48:23 +0600

    Update DisabledReports.kt

--- app/src/main/java/ani/dantotsu/others/DisabledReports.kt
+++ app/src/main/java/ani/dantotsu/others/DisabledReports.kt
@@ -1,5 +1,5 @@
 package ani.dantotsu.others
 
-const val DisabledReports = false
+const val DisabledReports = true
 //Setting this to false, will allow sending crash reports to Dantotsu's Firebase Crashlytics
 //if you want a custom build without crash reporting, set this to true
