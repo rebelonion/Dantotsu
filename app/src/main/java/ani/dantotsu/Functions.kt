600137766364c72035854b725bbd188fbab94c86
aayush262 <aayushthakur262006@gmail.com>
2024-01-15 23:39:52 +0530

    auto hide android navbar

--- app/src/main/java/ani/dantotsu/Functions.kt
+++ app/src/main/java/ani/dantotsu/Functions.kt
@@ -157,11 +157,22 @@ fun initActivity(a: Activity) {
                 }
         }
         a.hideStatusBar()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P && statusBarHeight == 0 && a.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            window.decorView.rootWindowInsets?.displayCutout?.apply {
-                if (boundingRects.size > 0) {
-                    statusBarHeight = min(boundingRects[0].width(), boundingRects[0].height())
-                }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R &&
+            statusBarHeight == 0 &&
+            a.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT
+        ) {
+            window.setDecorFitsSystemWindows(false)
+
+            window.decorView.setOnApplyWindowInsetsListener { _, insets ->
+                statusBarHeight = insets.getInsets(WindowInsetsCompat.Type.systemBars()).top
+                navBarHeight = insets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom
+                insets
+            }
+
+            window.insetsController?.let { controller ->
+                controller.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                controller.hide(WindowInsetsCompat.Type.navigationBars())
             }
         }
     } else
