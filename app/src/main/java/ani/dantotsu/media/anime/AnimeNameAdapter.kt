9416c88511e0c33e3ad2ff6138fd373eb57d5011
rebelonion <87634197+rebelonion@users.noreply.github.com>
2024-01-17 20:37:49 -0600

    regex fix

--- app/src/main/java/ani/dantotsu/media/anime/AnimeNameAdapter.kt
+++ app/src/main/java/ani/dantotsu/media/anime/AnimeNameAdapter.kt
@@ -6,7 +6,9 @@ import java.util.regex.Pattern
 class AnimeNameAdapter {
     companion object {
         const val episodeRegex =
-            "(episode|ep|e)[\\s:.\\-]*([\\d]+\\.?[\\d]*)[\\s:.\\-]*\\(?\\s*(sub|subbed|dub|dubbed)*\\s*\\)?\\s*|(\\b\\d+\\b)"
+            "(episode|ep|e)[\\s:.\\-]*([\\d]+\\.?[\\d]*)[\\s:.\\-]*\\(?\\s*(sub|subbed|dub|dubbed)*\\s*\\)?\\s*"
+        const val failedEpisodeNumberRegex =
+            "(?<!part\\s)\\b(\\d+)\\b"
         const val seasonRegex = "(season|s)[\\s:.\\-]*(\\d+)[\\s:.\\-]*"
 
         fun findSeasonNumber(text: String): Int? {
@@ -37,14 +39,32 @@ class AnimeNameAdapter {
 
         fun removeEpisodeNumber(text: String): String {
             val regexPattern = Regex(episodeRegex, RegexOption.IGNORE_CASE)
-            return text.replace(regexPattern, "").ifEmpty {
+            val removedNumber = text.replace(regexPattern, "").ifEmpty {
                 text
+            }
+            return if (removedNumber.equals(text, true)) {
+                val failedEpisodeNumberPattern: Regex = Regex(failedEpisodeNumberRegex, RegexOption.IGNORE_CASE)
+                failedEpisodeNumberPattern.replace(removedNumber) { mr ->
+                    mr.value.replaceFirst(mr.groupValues[1], "")
+                }.ifEmpty { removedNumber }
+            } else {
+                removedNumber
             }
         }
 
         fun removeEpisodeNumberCompletely(text: String): String {
             val regexPattern = Regex(episodeRegex, RegexOption.IGNORE_CASE)
-            return text.replace(regexPattern, "")
+            val removedNumber = text.replace(regexPattern, "").ifEmpty {
+                text
+            }
+            return if (removedNumber.equals(text, true)) {
+                val failedEpisodeNumberPattern: Regex = Regex(failedEpisodeNumberRegex, RegexOption.IGNORE_CASE)
+                failedEpisodeNumberPattern.replace(removedNumber) { mr ->
+                    mr.value.replaceFirst(mr.groupValues[1], "")
+                }.ifEmpty { removedNumber }
+            } else {
+                removedNumber
+            }
         }
     }
 }
