22e687b9d8db80075bbbbff8ceb0d27d7c884e4e
rebelonion <87634197+rebelonion@users.noreply.github.com>
2024-02-08 08:15:51 -0600

    feat: add more helpers/developers

--- app/src/main/java/ani/dantotsu/settings/DevelopersDialogFragment.kt
+++ app/src/main/java/ani/dantotsu/settings/DevelopersDialogFragment.kt
@@ -20,22 +20,40 @@ class DevelopersDialogFragment : BottomSheetDialogFragment() {
             "https://github.com/rebelonion"
         ),
         Developer(
-            "Wai What",
-            "https://avatars.githubusercontent.com/u/149729762?v=4",
-            "Icon Designer",
-            "https://github.com/WaiWhat"
-        ),
-        Developer(
             "Aayush262",
             "https://avatars.githubusercontent.com/u/99584765?v=4",
             "Contributor",
             "https://github.com/aayush2622"
         ),
         Developer(
+            "Sadwhy",
+            "https://avatars.githubusercontent.com/u/99601717?v=4",
+            "Contributor",
+            "https://github.com/Sadwhy"
+        ),
+        Developer(
+            "Wai What",
+            "https://avatars.githubusercontent.com/u/149729762?v=4",
+            "Icon Designer",
+            "https://github.com/WaiWhat"
+        ),
+        Developer(
             "MarshMeadow",
             "https://avatars.githubusercontent.com/u/88599122?v=4",
             "Beta Icon Designer",
             "https://github.com/MarshMeadow?tab=repositories"
+        ),
+        Developer(
+            "Zaxx69",
+            "https://avatars.githubusercontent.com/u/138523882?v=4",
+            "Telegram Admin",
+            "https://github.com/Zaxx69"
+        ),
+        Developer(
+            "Arif Alam",
+            "https://avatars.githubusercontent.com/u/70383209?v=4",
+            "Head Discord Moderator",
+            "https://youtube.com/watch?v=dQw4w9WgXcQ"
         ),
     )
 
