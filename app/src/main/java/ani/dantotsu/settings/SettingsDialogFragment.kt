2742f58af510ee4b3d4d15790a7f5f17928125b5
ibo <41344259+sneazy-ibo@users.noreply.github.com>
2024-03-08 09:34:32 +0100

    feat(): fixed the UI changes 🦍 + notificationIcon logic and long press userAvatar 🐒 (#226)

--- app/src/main/java/ani/dantotsu/settings/SettingsDialogFragment.kt
+++ app/src/main/java/ani/dantotsu/settings/SettingsDialogFragment.kt
@@ -59,6 +59,13 @@ class SettingsDialogFragment : BottomSheetDialogFragment() {
         val theme = requireContext().theme
         theme.resolveAttribute(com.google.android.material.R.attr.colorSurface, typedValue, true)
         window?.navigationBarColor = typedValue.data
+        val notificationIcon = if (Anilist.unreadNotificationCount > 0) {
+            R.drawable.ic_round_notifications_active_24
+        } else {
+            R.drawable.ic_round_notifications_none_24
+        }
+        binding.settingsNotification.setImageResource(notificationIcon)
+        binding.settingsNotification.visibility = View.VISIBLE
 
         if (Anilist.token != null) {
             binding.settingsLogin.setText(R.string.logout)
