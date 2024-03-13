d4f5e17863af2122796f9062cfdfc3d91b269dd9
TwistedUmbrellaX <twistedumbrella@gmail.com>
2024-03-13 15:11:51 -0400

    fix: forgotten uncle onRestart
    It functions a lot like onResume, but assumes that onCreate ran and the user navigated away from the activity completely.

--- app/src/main/java/ani/dantotsu/MainActivity.kt
+++ app/src/main/java/ani/dantotsu/MainActivity.kt
@@ -49,7 +49,6 @@ import ani.dantotsu.settings.saving.PrefName
 import ani.dantotsu.settings.saving.SharedPreferenceBooleanLiveData
 import ani.dantotsu.subcriptions.Subscription.Companion.startSubscription
 import ani.dantotsu.themes.ThemeManager
-import ani.dantotsu.util.Logger
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 import eu.kanade.domain.source.service.SourcePreferences
@@ -208,6 +207,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.root.doOnAttach {
             initActivity(this)
+            window.navigationBarColor = getColor(android.R.color.transparent)
             selectedOption = if (fragment != null) {
                 when (fragment) {
                     AnimeFragment::class.java.name -> 0
@@ -355,9 +355,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        initActivity(this)
+    override fun onRestart() {
+        super.onRestart()
         window.navigationBarColor = getColor(android.R.color.transparent)
     }
 
