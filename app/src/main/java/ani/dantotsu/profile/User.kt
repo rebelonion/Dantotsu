3666758e6ec2bf1fb63b290abeeaf92c5b4b4d6a
aayush262 <aayushthakur262006@gmail.com>
2024-04-07 21:31:15 +0530

    feat(socials): ratings and progress

--- app/src/main/java/ani/dantotsu/profile/User.kt
+++ app/src/main/java/ani/dantotsu/profile/User.kt
@@ -5,5 +5,10 @@ data class User(
     val name: String,
     val pfp: String?,
     val banner: String?,
-    val info: String? = null,
+    // for media
+    val status: String? = null,
+    val score: Float? = null,
+    val progress: Int? = null,
+    val totalEpisodes : Int? = null,
+    val nextAiringEpisode : Int? = null,
 )
