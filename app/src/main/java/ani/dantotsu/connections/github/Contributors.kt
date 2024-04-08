c242d9dd99fa35f2a7e54689ade7d0cd272b99e8
aayush262 <aayushthakur262006@gmail.com>
2024-04-08 22:52:13 +0530

    fix: no more 13 arifs

--- app/src/main/java/ani/dantotsu/connections/github/Contributors.kt
+++ app/src/main/java/ani/dantotsu/connections/github/Contributors.kt
@@ -22,52 +22,80 @@ class Contributors {
                 .parsed<JsonArray>().map {
                     Mapper.json.decodeFromJsonElement<GithubResponse>(it)
                 }
-            res.find { it.login == "rebelonion"}?.let { first ->
-                developers = developers.plus(
-                    Developer(
-                        first.login,
-                        first.avatarUrl,
-                         "Owner and Maintainer",
-                        first.htmlUrl
-                    )
-                )
-            }
-            res.filter {it.login != "rebelonion"}.forEach {
+            res.forEach {
+                val role = when (it.login) {
+                    "rebelonion" -> "Owner & Maintainer"
+                    "sneazy-ibo" -> "Contributor & Comment Moderator"
+                    "WaiWhat" -> "Icon Designer"
+                    else -> "Contributor"
+                }
                 developers = developers.plus(
                     Developer(
                         it.login,
                         it.avatarUrl,
-                        "Contributor",
+                        role,
                         it.htmlUrl
                     )
                 )
             }
-            developers = developers.plus((arrayOf(
-                Developer(
-                    "Wai What",
-                    "https://avatars.githubusercontent.com/u/149729762?v=4",
-                    "Icon Designer",
-                    "https://github.com/WaiWhat"
-                ),
-                Developer(
-                    "MarshMeadow",
-                    "https://avatars.githubusercontent.com/u/88599122?v=4",
-                    "Beta Icon Designer",
-                    "https://github.com/MarshMeadow?tab=repositories"
-                ),
-                Developer(
-                    "Zaxx69",
-                    "https://avatars.githubusercontent.com/u/138523882?v=4",
-                    "Telegram Admin",
-                    "https://github.com/Zaxx69"
-                ),
-                Developer(
-                    "Arif Alam",
-                    "https://avatars.githubusercontent.com/u/70383209?v=4",
-                    "Head Discord Moderator",
-                    "https://youtube.com/watch?v=dQw4w9WgXcQ"
+            developers = developers.plus(
+                arrayOf(
+                    Developer(
+                        "MarshMeadow",
+                        "https://avatars.githubusercontent.com/u/88599122?v=4",
+                        "Beta Icon Designer & Website Maintainer",
+                        "https://github.com/MarshMeadow?tab=repositories"
+                    ),
+                    Developer(
+                        "Zaxx69",
+                        "https://s4.anilist.co/file/anilistcdn/user/avatar/large/b6342562-kxE8m4i7KUMK.png",
+                        "Telegram Admin",
+                        "https://anilist.co/user/6342562"
+                    ),
+                    Developer(
+                        "Arif Alam",
+                        "https://s4.anilist.co/file/anilistcdn/user/avatar/large/b6011177-2n994qtayiR9.jpg",
+                        "Discord & Comment Moderator",
+                        "https://anilist.co/user/6011177"
+                    ),
+                    Developer(
+                        "SunglassJeery",
+                        "https://s4.anilist.co/file/anilistcdn/user/avatar/large/b5804776-FEKfP5wbz2xv.png",
+                        "Discord & Comment Moderator",
+                        "https://anilist.co/user/5804776"
+                    ),
+                    Developer(
+                        "Excited",
+                        "https://s4.anilist.co/file/anilistcdn/user/avatar/large/b6131921-toSoGWmKbRA1.png",
+                        "Comment Moderator",
+                        "https://anilist.co/user/6131921"
+                    ),
+                    Developer(
+                        "Gurjshan",
+                        "https://s4.anilist.co/file/anilistcdn/user/avatar/large/b6363228-rWQ3Pl3WuxzL.png",
+                        "Comment Moderator",
+                        "https://anilist.co/user/6363228"
+                    ),
+                    Developer(
+                        "NekoMimi",
+                        "https://s4.anilist.co/file/anilistcdn/user/avatar/large/b6244220-HOpImMGMQAxW.jpg",
+                        "Comment Moderator",
+                        "https://anilist.co/user/6244220"
+                    ),
+                    Developer(
+                        "Zaidsenior",
+                        "https://s4.anilist.co/file/anilistcdn/user/avatar/large/b6049773-8cjYeUOFUguv.jpg",
+                        "Comment Moderator",
+                        "https://anilist.co/user/6049773"
+                    ),
+                    Developer(
+                        "hastsu",
+                        "https://cdn.discordapp.com/avatars/602422545077108749/20b4a6efa4314550e4ed51cdbe4fef3d.webp?size=160",
+                        "Comment Moderator",
+                        "https://anilist.co/user/6183359"
+                    ),
                 )
-            )))
+            )
         }
         return developers
     }
