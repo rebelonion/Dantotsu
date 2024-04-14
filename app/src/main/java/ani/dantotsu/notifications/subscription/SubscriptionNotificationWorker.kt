272e159f07f6fa7cd74feae5f971fd6746f8ed45
itsmechinmoy <95120262+itsmechinmoy@users.noreply.github.com>
2024-04-14 21:22:22 +0530

    Update SubscriptionNotificationWorker.kt

--- app/src/main/java/ani/dantotsu/notifications/subscription/SubscriptionNotificationWorker.kt
+++ app/src/main/java/ani/dantotsu/notifications/subscription/SubscriptionNotificationWorker.kt
@@ -1,27 +1,24 @@
-package ani.dantotsu.notifications.subscription
+package ani.dantotsu.notifications.comment
 
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import ani.dantotsu.notifications.anilist.AnilistNotificationTask
 import ani.dantotsu.util.Logger
 
-class SubscriptionNotificationWorker(appContext: Context, workerParams: WorkerParameters) :
+class CommentNotificationWorker(appContext: Context, workerParams: WorkerParameters) :
     CoroutineWorker(appContext, workerParams) {
-
     override suspend fun doWork(): Result {
-        Logger.log("SubscriptionNotificationWorker: doWork")
-        return if (AnilistNotificationTask().execute(applicationContext)) {
+        Logger.log("CommentNotificationWorker: doWork")
+        return if (CommentNotificationTask().execute(applicationContext)) {
             Result.success()
         } else {
-            Logger.log("SubscriptionNotificationWorker: doWork failed")
+            Logger.log("CommentNotificationWorker: doWork failed")
             Result.retry()
         }
     }
 
     companion object {
         val checkIntervals = arrayOf(0L, 480, 720, 1440)
-        const val WORK_NAME =
-            "ani.dantotsu.notifications.subscription.SubscriptionNotificationWorker"
+        const val WORK_NAME = "ani.dantotsu.notifications.comment.CommentNotificationWorker"
     }
 }
