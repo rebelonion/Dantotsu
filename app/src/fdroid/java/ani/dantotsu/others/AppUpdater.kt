e7a60e07d8cf94cabb421b871eff4709a4bcb04a
rebelonion <87634197+rebelonion@users.noreply.github.com>
2024-05-24 14:03:33 -0500

    fix: destroyed activity crash on slower phones

--- app/src/fdroid/java/ani/dantotsu/others/AppUpdater.kt
+++ app/src/fdroid/java/ani/dantotsu/others/AppUpdater.kt
@@ -6,4 +6,27 @@ object AppUpdater {
     suspend fun check(activity: FragmentActivity, post: Boolean = false) {
         //no-op
     }
+
+    @Serializable
+    data class GithubResponse(
+        @SerialName("html_url")
+        val htmlUrl: String,
+        @SerialName("tag_name")
+        val tagName: String,
+        val prerelease: Boolean,
+        @SerialName("created_at")
+        val createdAt: String,
+        val body: String? = null,
+        val assets: List<Asset>? = null
+    ) {
+        @Serializable
+        data class Asset(
+            @SerialName("browser_download_url")
+            val browserDownloadURL: String
+        )
+
+        fun timeStamp(): Long {
+            return dateFormat.parse(createdAt)!!.time
+        }
+    }
 }
